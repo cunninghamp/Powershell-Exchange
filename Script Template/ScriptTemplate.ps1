ba962fbc9fc549e3fce37f07302fc43cb81dc1c2
Paul Cunningham <paul@locklan.com.au>
2014-10-14 15:22:13 +1000

    ScriptTemplate.ps1

---
+++ Script Template/ScriptTemplate.ps1
@@ -0,0 +1,152 @@
+﻿<#
+.SYNOPSIS
+SCRIPTNAME.ps1
+
+.DESCRIPTION 
+
+
+.OUTPUTS
+
+
+.PARAMETER
+
+
+.PARAMETER
+
+
+.PARAMETER
+
+
+.EXAMPLE
+
+
+.EXAMPLE
+
+
+.EXAMPLE
+
+
+.LINK
+
+
+.NOTES
+Written by: Paul Cunningham
+
+Find me on:
+
+* Blog:		http://paulcunningham.me
+* Twitter:	https://twitter.com/paulcunningham
+* LinkedIn:	http://au.linkedin.com/in/cunninghamp/
+* Github:	https://github.com/cunninghamp
+
+For more Exchange Server tips, tricks and news
+check out Exchange Server Pro.
+
+* Website:	http://exchangeserverpro.com
+* Twitter:	http://twitter.com/exchservpro
+
+Change Log:
+V1.00, DD/MM/YYYY - Initial version
+#>
+
+#requires -version 2
+
+[CmdletBinding()]
+param (
+	[Parameter( Mandatory=$false)]
+	[string]$PARAMETERNAME,
+
+	[Parameter( Mandatory=$false)]
+	[switch]$Log
+	)
+
+
+#...................................
+# Variables
+#...................................
+
+$scriptname = $MyInvocation.ScriptName
+$now = Get-Date
+
+
+#...................................
+# Logfile Strings
+#...................................
+
+$logstring0 = "====================================="
+$logstring1 = " $scriptname"
+
+#...................................
+# Initialization Strings
+#...................................
+
+$initstring0 = "Initializing..."
+$initstring1 = "Loading the Exchange Server PowerShell snapin"
+$initstring2 = "The Exchange Server PowerShell snapin did not load."
+$initstring3 = "Setting scope to entire forest"
+
+
+#...................................
+# Functions
+#...................................
+
+#This function is used to write the log file if -Log is used
+Function Write-Logfile()
+{
+	param( $logentry )
+	$timestamp = Get-Date -DisplayHint Time
+	"$timestamp $logentry" | Out-File $logfile -Append
+}
+
+
+#...................................
+# Initialize
+#...................................
+
+#Log file is overwritten each time the script is run to avoid
+#very large log files from growing over time
+if ($Log) {
+	$timestamp = Get-Date -DisplayHint Time
+	"$timestamp $logstring0" | Out-File $logfile
+	Write-Logfile $logstring1
+	Write-Logfile "  $now"
+	Write-Logfile $logstring0
+}
+
+Write-Host $initstring0
+if ($Log) {Write-Logfile $initstring0}
+
+#Add Exchange 2010 snapin if not already loaded in the PowerShell session
+if (!(Get-PSSnapin | where {$_.Name -eq "Microsoft.Exchange.Management.PowerShell.E2010"}))
+{
+	Write-Verbose $initstring1
+	if ($Log) {Write-Logfile $initstring1}
+	try
+	{
+		Add-PSSnapin Microsoft.Exchange.Management.PowerShell.E2010 -ErrorAction STOP
+	}
+	catch
+	{
+		#Snapin was not loaded
+		Write-Verbose $initstring2
+		if ($Log) {Write-Logfile $initstring2}
+		Write-Warning $_.Exception.Message
+		EXIT
+	}
+	. $env:ExchangeInstallPath\bin\RemoteExchange.ps1
+	Connect-ExchangeServer -auto -AllowClobber
+}
+
+
+#Set scope to include entire forest
+Write-Verbose $initstring3
+if ($Log) {Write-Logfile $initstring3}
+if (!(Get-ADServerSettings).ViewEntireForest)
+{
+	Set-ADServerSettings -ViewEntireForest $true -WarningAction SilentlyContinue
+}
+
+
+#...................................
+# Script
+#...................................
