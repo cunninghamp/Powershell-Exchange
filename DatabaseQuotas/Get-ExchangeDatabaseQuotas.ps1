dc679005485d1de0c68d8a9fa5e55873d7ca956a
Paul Cunningham <paul@locklan.com.au>
2018-10-22 09:39:02 +1000

    Update Get-ExchangeDatabaseQuotas.ps1

--- DatabaseQuotas/Get-ExchangeDatabaseQuotas.ps1
+++ DatabaseQuotas/Get-ExchangeDatabaseQuotas.ps1
@@ -21,16 +21,10 @@ Written by: Paul Cunningham
 
 Find me on:
 
-* My Blog:	http://paulcunningham.me
+* My Blog:	https://paulcunningham.me
 * Twitter:	https://twitter.com/paulcunningham
-* LinkedIn:	http://au.linkedin.com/in/cunninghamp/
+* LinkedIn:	https://au.linkedin.com/in/cunninghamp/
 * Github:	https://github.com/cunninghamp
-
-For more Exchange Server tips, tricks and news
-check out Exchange Server Pro.
-
-* Website:	http://exchangeserverpro.com
-* Twitter:	http://twitter.com/exchservpro
 
 Change Log
 V1.00, 11/03/2014 - Initial Version
